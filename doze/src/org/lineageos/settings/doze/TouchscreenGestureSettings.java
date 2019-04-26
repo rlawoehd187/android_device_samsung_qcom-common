74a98e155ec8cc3b1831e636f241907212394560
Gegham Zakaryan <zakaryan.2004@outlook.com>
2019-04-26 16:00:21 +0400

    qcom-common: Pixel Experience bringup
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- doze/src/org/lineageos/settings/doze/TouchscreenGestureSettings.java
+++ doze/src/org/lineageos/settings/doze/TouchscreenGestureSettings.java
@@ -24,8 +24,6 @@ import android.support.v7.preference.Preference;
 import android.provider.Settings;
 import android.view.MenuItem;
 
-import org.lineageos.internal.util.ScreenType;
-
 public class TouchscreenGestureSettings extends PreferenceFragment {
 
     private static final String KEY_HAND_WAVE = "gesture_hand_wave";
@@ -52,10 +50,8 @@ public class TouchscreenGestureSettings extends PreferenceFragment {
     public void onResume() {
         super.onResume();
 
-        // If running on a phone, remove padding around the listview
-        if (!ScreenType.isTablet(getContext())) {
-            getListView().setPadding(0, 0, 0, 0);
-        }
+        // Always on a phone (J5), so remove padding around the listview
+        getListView().setPadding(0, 0, 0, 0);
     }
 
     private Preference.OnPreferenceChangeListener mProximityListener =
